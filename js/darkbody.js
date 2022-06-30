b293f491502ddc99ecfcaaf2de251947e2ee2f68
TranThiKieuTrang <tranthikieuden@gmail.com>
2022-06-30 22:27:48 +0700

    add progress bar

--- js/darkbody.js
+++ js/darkbody.js
@@ -1,3 +1,4 @@
 document.getElementById("switchButton").onclick = function () {
     document.getElementById("darkBody").classList.toggle("dark");
+    document.body.classList.toggle("dark-theme");
 };
