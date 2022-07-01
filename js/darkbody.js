775b1ae0820fb7bb81b82ee683554169d59fe921
TranThiKieuTrang <tranthikieuden@gmail.com>
2022-07-01 15:16:48 +0700

    add dark theme

--- js/darkbody.js
+++ js/darkbody.js
@@ -1,6 +1,6 @@
-document.getElementById("switchButton").onclick = function () {
-    document.getElementById("darkBody").classList.toggle("dark");
-    document.body.classList.toggle("dark-theme");
+document.getElementById("checkbox").onclick = function () {
+  document.getElementById("darkBody").classList.toggle("dark");
+  document.body.classList.toggle("dark-theme");
 };
 
 $(document).ready(function () {
@@ -45,4 +45,83 @@ $(document).ready(function () {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
+
+  var $bannerSlider = jQuery(".banner-slider");
+  var $bannerFirstSlide = $("div.banner-slide:first-child");
+
+  $bannerSlider.on("init", function (e, slick) {
+    var $firstAnimatingElements = $bannerFirstSlide.find("[data-animation]");
+    slideanimate($firstAnimatingElements);
+  });
+  $bannerSlider.on(
+    "beforeChange",
+    function (e, slick, currentSlide, nextSlide) {
+      var $animatingElements = $(
+        'div.slick-slide[data-slick-index="' + nextSlide + '"]'
+      ).find("[data-animation]");
+      slideanimate($animatingElements);
+    }
+  );
+  $bannerSlider.slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    dots: false,
+    swipe: true,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: false,
+          autoplaySpeed: 4000,
+          swipe: true,
+        },
+      },
+    ],
+  });
+  function slideanimate(elements) {
+    var animationEndEvents =
+      "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+    elements.each(function () {
+      var $this = $(this);
+      var $animationDelay = $this.data("delay");
+      var $animationType = "animated " + $this.data("animation");
+      $this.css({
+        "animation-delay": $animationDelay,
+        "-webkit-animation-delay": $animationDelay,
+      });
+      $this.addClass($animationType).one(animationEndEvents, function () {
+        $this.removeClass($animationType);
+      });
+    });
+  }
+
+  $(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 80) {
+      $("#site-header").addClass("nav-fixed");
+    } else {
+      $("#site-header").removeClass("nav-fixed");
+    }
+  });
+
+  //Main navigation Active Class Add Remove
+  $(".navbar-toggler").on("click", function () {
+    $("header").toggleClass("active");
+  });
+  $(document).on("ready", function () {
+    if ($(window).width() > 991) {
+      $("header").removeClass("active");
+    }
+    $(window).on("resize", function () {
+      if ($(window).width() > 991) {
+        $("header").removeClass("active");
+      }
+    });
+  });
 });
